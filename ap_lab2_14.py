7159fb4eba6179e75b0a5da4b0ea8e8cc2c969db
Sneha <snehamittal88293@gmail.com>
2022-01-15 12:07:54 +0530

    cost price of item when selling pprice and profit are given

---
+++ ap_lab2_14.py
@@ -0,0 +1,4 @@
+selling_price = int(input("enter the selling price: "))
+profit_earn = int(input("earn profit: "))
+cost_price = (selling_price - profit_earn)/15
+print(f"cost price of one item = {round(cost_price,2)} ")
